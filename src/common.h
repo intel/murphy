94dc4571e94e59fb60fef242d871fb2e4f3061d2
Krisztian Litkey <krisztian.litkey@intel.com>
2012-04-21 12:15:56 +0300

    common,core: added top-level convenience include files.

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#ifndef __MURPHY_COMMON_H__
+#define __MURPHY_COMMON_H__
+
+#include <murphy/common/macros.h>
+#include <murphy/common/mm.h>
+#include <murphy/common/log.h>
+#include <murphy/common/list.h>
+#include <murphy/common/mainloop.h>
+#include <murphy/common/hashtbl.h>
+#include <murphy/common/utils.h>
+#include <murphy/common/file-utils.h>
+
+#endif
---
+++ src/core.h
@@ -0,0 +1,7 @@
+#ifndef __MURPHY_CORE_H__
+#define __MURPHY_CORE_H__
+
+#include <murphy/core/context.h>
+#include <murphy/core/plugin.h>
+
+#endif
