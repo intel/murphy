36cdbfd293b2d52c478aed66d490cf71733d3418
Krisztian Litkey <krisztian.litkey@intel.com>
2012-09-05 14:13:14 +0300

    resolver: removed unused/moved context typedefs.

--- src/resolver/resolver-types.h
+++ src/resolver/resolver-types.h
@@ -32,41 +32,6 @@ struct fact_s {
 };
 
 
-/*
- * a context variable (used to pass contextual data to resolver targets)
- */
-typedef struct {
-    const char        *name;             /* variable name */
-    mrp_script_type_t  type;             /* type if declared */
-    int                id;               /* variable id */
-} context_var_t;
-
-
-/*
- * a context frame (a set of context variable values)
- */
-typedef struct context_value_s context_value_t;
-struct context_value_s {
-    int                 id;              /* variable id */
-    mrp_script_value_t  value;           /* value for this variable */
-    context_value_t    *next;            /* next value in this frame */
-};
-
-typedef struct context_frame_s context_frame_t;
-struct context_frame_s {
-    context_value_t *values;             /* hook to more value */
-    context_frame_t *prev;               /* previous frame */
-};
-
-
-struct mrp_context_tbl_s {
-    context_var_t   *variables;          /* known/declared context variables */
-    int              nvariable;          /* number of variables */
-    mrp_htbl_t      *names;              /* variable name to id mapping */
-    context_frame_t *frame;              /* active frame */
-};
-
-
 struct mrp_resolver_s {
     target_t          *targets;          /* targets defined in the ruleset */
     int                ntarget;          /* number of targets */
