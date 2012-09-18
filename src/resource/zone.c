6e82e12561821053b39a7a8c9c03367c4ad26e3b
Janos Kovacs <jankovac503@gmail.com>
2012-09-18 03:33:27 +0300

    resource: add database table for zones

--- src/resource/zone.c
+++ src/resource/zone.c
@@ -29,6 +29,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 #include <murphy/common/mm.h>
 #include <murphy/common/log.h>
@@ -37,10 +38,13 @@
 #include <murphy/resource/client-api.h>
 #include <murphy/resource/config-api.h>
 
+#include <murphy-db/mqi.h>
+
 #include "zone.h"
 
 
 #define ATTRIBUTE_MAX  32
+#define NAME_LENGTH    24
 
 #define VALID_TYPE(t) ((t) == mqi_string  || \
                        (t) == mqi_integer || \
@@ -48,9 +52,24 @@
                        (t) == mqi_floating  )
 
 
+#define ZONE_ID_IDX          0
+#define ZONE_NAME_IDX        1
+#define FIRST_ATTRIBUTE_IDX  2
+
+typedef struct {
+    uint32_t          zone_id;
+    const char       *zone_name;
+    mrp_attr_value_t  attrs[MQI_COLUMN_MAX];
+} zone_row_t;
+
 static mrp_zone_def_t *zone_def;
 static uint32_t        zone_count;
 static mrp_zone_t     *zone_table[MRP_ZONE_MAX];
+static mqi_handle_t    db_table = MQI_HANDLE_INVALID;
+
+static mqi_handle_t create_zone_table(mrp_zone_def_t *);
+static void insert_into_zone_table(mrp_zone_t *);
+static void set_attr_descriptors(mqi_column_desc_t *);
 
 
 int mrp_zone_definition_create(mrp_attr_def_t *attrdefs)
@@ -74,6 +93,8 @@ int mrp_zone_definition_create(mrp_attr_def_t *attrdefs)
 
     if (mrp_attribute_copy_definitions(attrdefs, def->attrdefs) < 0)
         return -1;
+
+    db_table = create_zone_table(def);
 
     return 0;
 }
@@ -120,6 +141,8 @@ uint32_t mrp_zone_create(const char *name, mrp_attr_t *attrs)
         mrp_log_error("Memory alloc failure. Can't create zone '%s'", name);
         return MRP_ZONE_ID_INVALID;
     }
+
+    insert_into_zone_table(zone);
 
     zone_table[zone->id] = zone;
 
@@ -237,6 +260,114 @@ int mrp_zone_attribute_print(mrp_zone_t *zone, char *buf, int len)
 
     return mrp_attribute_print(zone_def->nattr, zone_def->attrdefs,
                                zone->attrs, buf,len);
+}
+
+
+static mqi_handle_t create_zone_table(mrp_zone_def_t *zdef)
+{
+    MQI_COLUMN_DEFINITION_LIST(base_coldefs,
+        MQI_COLUMN_DEFINITION( "zone_id"       , MQI_UNSIGNED            , 0 ),
+        MQI_COLUMN_DEFINITION( "zone_name"     , MQI_VARCHAR(NAME_LENGTH), 0 )
+    );
+
+    MQI_INDEX_DEFINITION(indexdef,
+        MQI_INDEX_COLUMN("zone_id")
+    );
+
+    char *name;
+    mqi_column_def_t coldefs[MQI_COLUMN_MAX + 1];
+    mqi_column_def_t *col;
+    mrp_attr_def_t *atd;
+    mqi_handle_t table;
+    size_t i,j;
+
+    MRP_ASSERT(zdef, "invalid argument");
+    MRP_ASSERT(zdef->nattr < MQI_COLUMN_MAX, "too many zone attributes");
+    MRP_ASSERT(db_table == MQI_HANDLE_INVALID,
+               "multiple definition of zone data table");
+
+    mqi_open();
+
+    name = "zones";
+
+    j = MQI_DIMENSION(base_coldefs) - 1;
+    memcpy(coldefs, base_coldefs, j * sizeof(mqi_column_def_t));
+
+    for (i = 0;  i < zdef->nattr && j < MQI_COLUMN_MAX;  i++, j++) {
+        col = coldefs + j;
+        atd = zdef->attrdefs + i;
+
+        col->name   = atd->name;
+        col->type   = atd->type;
+        col->length = (col->type == mqi_string) ? NAME_LENGTH : 0;
+        col->flags  = 0;
+    }
+
+    memset(coldefs + j, 0, sizeof(mqi_column_def_t));
+
+    table = MQI_CREATE_TABLE(name, MQI_TEMPORARY, coldefs, indexdef);
+
+    if (table == MQI_HANDLE_INVALID)
+        mrp_log_error("Can't create table '%s': %s", name, strerror(errno));
+
+    return table;
+}
+
+static void insert_into_zone_table(mrp_zone_t *zone)
+{
+    uint32_t i;
+    int n;
+    zone_row_t row;
+    zone_row_t *rows[2];
+    mqi_column_desc_t cdsc[FIRST_ATTRIBUTE_IDX + MQI_COLUMN_MAX + 1];
+
+    MRP_ASSERT(zone_def, "no zone definition");
+    MRP_ASSERT(db_table != MQI_HANDLE_INVALID, "no zone table");
+    MRP_ASSERT(FIRST_ATTRIBUTE_IDX + zone_def->nattr <= MQI_COLUMN_MAX,
+               "too many attributes for a table");
+
+    row.zone_id    = zone->id;
+    row.zone_name  = zone->name;
+    memcpy(row.attrs, zone->attrs, zone_def->nattr * sizeof(mrp_attr_value_t));
+
+    i = 0;
+    cdsc[i].cindex = ZONE_ID_IDX;
+    cdsc[i].offset = MQI_OFFSET(zone_row_t, zone_id);
+
+    i++;
+    cdsc[i].cindex = ZONE_NAME_IDX;
+    cdsc[i].offset = MQI_OFFSET(zone_row_t, zone_name);
+
+    set_attr_descriptors(cdsc + (i+1));
+
+    rows[0] = &row;
+    rows[1] = NULL;
+
+    if ((n = MQI_INSERT_INTO(db_table, cdsc, rows)) != 1)
+        mrp_log_error("can't insert row into zone table");
+}
+
+static void set_attr_descriptors(mqi_column_desc_t *cdsc)
+{
+    uint32_t i,j;
+    int o;
+
+    for (i = j = 0;  j < zone_def->nattr;  j++) {
+        switch (zone_def->attrdefs[j].type) {
+        case mqi_string:   o = MQI_OFFSET(zone_row_t,attrs[j].string);   break;
+        case mqi_integer:  o = MQI_OFFSET(zone_row_t,attrs[j].integer);  break;
+        case mqi_unsignd:  o = MQI_OFFSET(zone_row_t,attrs[j].unsignd);  break;
+        case mqi_floating: o = MQI_OFFSET(zone_row_t,attrs[j].floating); break;
+        default:           /* skip this */                            continue;
+        }
+
+        cdsc[i].cindex = FIRST_ATTRIBUTE_IDX + j;
+        cdsc[i].offset = o;
+        i++;
+    }
+
+    cdsc[i].cindex = -1;
+    cdsc[i].offset =  1;
 }
 
 
