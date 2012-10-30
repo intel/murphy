6391c2621a8a1fced1460255d28a49e448a291a0
Krisztian Litkey <krisztian.litkey@intel.com>
2012-10-30 11:29:39 +0200

    plugins: removed decision-proto plugin.

--- src/plugins/decision-proto/notify.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MURPHY_DECISION_NOTIFY_H__
-#define __MURPHY_DECISION_NOTIFY_H__
-
-#include "decision-types.h"
-
-void notify_table_changes(pdp_t *pdp);
-
-#endif /* __MURPHY_DECISION_NOTIFY_H__ */
