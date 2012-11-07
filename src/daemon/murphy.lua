2004c801d124b1445318e009b85187322c7a2afa
Krisztian Litkey <krisztian.litkey@intel.com>
2012-11-07 22:48:11 +0200

    daemon: lua-related test config updates.

--- src/daemon/murphy.lua
+++ src/daemon/murphy.lua
@@ -25,9 +25,23 @@ m:try_load_plugin('glib')
 -- try loading the signalling plugin
 m:try_load_plugin('signalling', { address = 'internal:signalling' })
 
+-- load the native resource plugin
+if m:plugin_exists('resource-native') then
+    m:load_plugin('resource-native')
+else
+    m:info("No native resource plugin found...")
+end
+
 -- load the domain control plugin if it exists
 if m:plugin_exists('domain-control') then
     m:load_plugin('domain-control')
 else
     m:info("No domain-control plugin found...")
 end
+
+mdb.select {
+           name = "speed",
+           table = "audio_playback_owner",
+           columns = {"application_class"},
+           condition = "key = 'driver'"
+}
