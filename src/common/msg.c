092d538bbeff718bcd53b40d29a4abaf0e4f5053
Krisztian Litkey <krisztian.litkey@intel.com>
2012-11-27 14:55:26 +0200

    common: get rid of uninitialized variable warnings.

--- src/common/msg.c
+++ src/common/msg.c
@@ -513,6 +513,7 @@ int mrp_msg_get(mrp_msg_t *msg, ...)
      */
 
     start = msg->fields.next;
+    found = FALSE;
 
     while ((tag = va_arg(ap, unsigned int)) != MRP_MSG_FIELD_INVALID) {
         type  = va_arg(ap, unsigned int);
@@ -627,6 +628,7 @@ int mrp_msg_iterate_get(mrp_msg_t *msg, void **it, ...)
      */
 
     start = (*it) ? (mrp_list_hook_t *)*it : msg->fields.next;
+    found = FALSE;
 
     while ((tag = va_arg(ap, unsigned int)) != MRP_MSG_FIELD_INVALID) {
         type  = va_arg(ap, unsigned int);
@@ -636,8 +638,10 @@ int mrp_msg_iterate_get(mrp_msg_t *msg, void **it, ...)
             typep = va_arg(ap, uint16_t *);
             valp  = va_arg(ap, mrp_msg_value_t *);
         }
-        else
+        else {
             typep = NULL;
+            valp  = NULL;
+        }
 
         for (p = start; p != start->prev; p = p->next) {
             if (p == &msg->fields)
