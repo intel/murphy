b074beafebc8036209d146c487fe607e2149ebf7
Krisztian Litkey <krisztian.litkey@intel.com>
2013-02-01 18:43:46 +0200

    resource-wrt: added forgotten header.

---
+++ src/plugins/resource-wrt/resource-wrt.h
@@ -0,0 +1,17 @@
+#ifndef __MURPHY_RESOURCE_WRT_H__
+#define __MURPHY_RESOURCE_WRT_H__
+
+#define RESWRT_QUERY_CLASSES   "query-classes"
+#define RESWRT_QUERY_ZONES     "query-zones"
+#define RESWRT_QUERY_RESOURCES "query-resources"
+
+#define RESWRT_CREATE_SET      "create"
+#define RESWRT_DESTROY_SET     "destroy"
+#define RESWRT_ACQUIRE_SET     "acquire"
+#define RESWRT_RELEASE_SET     "release"
+
+#define RESWRT_EVENT           "event"
+#define RESWRT_STATE_GRANTED   "acquire"
+#define RESWRT_STATE_RELEASE   "release"
+
+#endif /* __MURPHY_RESOURCE_WRT_H__ */
