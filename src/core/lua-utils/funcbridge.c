0655fa1a47e8c58e8995c15006d41f7c246c1b2f
Janos Kovacs <jankovac503@gmail.com>
2013-02-11 13:27:42 +0200

    lua-utils: add support for function arrays

--- src/core/lua-utils/funcbridge.c
+++ src/core/lua-utils/funcbridge.c
@@ -39,16 +39,26 @@
 #include <murphy/core/lua-utils/funcbridge.h>
 #include <murphy/core/lua-utils/object.h>
 
-#define FUNCBRIDGE_METATABLE  "LuaBook.funcbridge"
-#define USERDATA_METATABLE    "LuaBook.funcbridge.userdata"
+#define FUNCBRIDGE_METATABLE             "LuaBook.funcbridge"
+#define FUNCBRIDGE_USERDATA_METATABLE    "LuaBook.funcbridge.userdata"
 
+#define FUNCARRAY_METATABLE              "LuaBook.funcarray"
+#define FUNCARRAY_USERDATA_METATABLE     "LuaBook.funcarray.userdata"
 
 static mrp_funcbridge_t *create_funcbridge(lua_State *, int, int);
 static mrp_funcbridge_t *check_funcbridge(lua_State *, int);
-static int call_from_lua(lua_State *);
+static int call_funcbridge_from_lua(lua_State *);
 static int get_funcbridge_field(lua_State *);
 static int set_funcbridge_field(lua_State *);
 static int funcbridge_destructor(lua_State *);
+
+static int call_funcarray_from_lua(lua_State *);
+static int get_funcarray_field(lua_State *);
+static int set_funcarray_field(lua_State *);
+static mrp_funcarray_t *to_funcarray(lua_State *, int);
+static int funcarray_destructor(lua_State *);
+
+static int make_lua_call(lua_State *, mrp_funcbridge_t *, int);
 
 
 void mrp_create_funcbridge_class(lua_State *L)
@@ -58,13 +68,13 @@ void mrp_create_funcbridge_class(lua_State *L)
     };
 
     static const struct luaL_reg override_methods [] = {
-        { "__call"    , call_from_lua        },
-        { "__index"   , get_funcbridge_field },
-        { "__newindex", set_funcbridge_field },
-        {      NULL   ,     NULL             }
+        { "__call"    , call_funcbridge_from_lua },
+        { "__index"   , get_funcbridge_field     },
+        { "__newindex", set_funcbridge_field     },
+        {      NULL   ,     NULL                 }
     };
 
-    luaL_newmetatable(L, USERDATA_METATABLE);
+    luaL_newmetatable(L, FUNCBRIDGE_USERDATA_METATABLE);
     lua_pushliteral(L, "__index");
     lua_pushvalue(L, -2);
     lua_settable(L, -3);        /* metatable.__index = metatable */
@@ -84,6 +94,34 @@ void mrp_create_funcbridge_class(lua_State *L)
     lua_setmetatable(L, -2);
     lua_pop(L, 1);
 }
+
+
+void mrp_create_funcarray_class(lua_State *L)
+{
+    static const struct luaL_reg override_methods [] = {
+        { "__call"    , call_funcarray_from_lua },
+        { "__index"   , get_funcarray_field     },
+        { "__newindex", set_funcarray_field     },
+        {      NULL   ,     NULL                }
+    };
+
+    luaL_newmetatable(L, FUNCARRAY_USERDATA_METATABLE);
+    lua_pushliteral(L, "__index");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -3);        /* metatable.__index = metatable */
+    lua_pushcfunction(L, funcarray_destructor);
+    lua_setfield(L, -2, "__gc");
+    lua_pop(L, 1);
+
+    luaL_newmetatable(L, FUNCARRAY_METATABLE);
+    lua_pushliteral(L, "__index");
+    lua_pushvalue(L, -2);
+    lua_settable(L, -3);        /* metatable.__index = metatable */
+    luaL_openlib(L, NULL, override_methods, 0);
+    lua_pop(L, 1);
+}
+
+
 
 
 mrp_funcbridge_t *mrp_funcbridge_create_cfunc(lua_State *L, const char *name,
@@ -287,6 +325,110 @@ int mrp_funcbridge_push(lua_State *L, mrp_funcbridge_t *fb)
 }
 
 
+mrp_funcarray_t *mrp_funcarray_create(lua_State *L)
+{
+    int table;
+    mrp_funcarray_t *fa;
+
+    lua_createtable(L, 0, 1);
+    table = lua_gettop(L);
+
+    luaL_getmetatable(L, FUNCARRAY_METATABLE);
+    lua_setmetatable(L, table);
+
+    lua_pushliteral(L, "userdata");
+
+    fa = (mrp_funcarray_t *)lua_newuserdata(L,sizeof(mrp_funcarray_t));
+    memset(fa, 0, sizeof(mrp_funcarray_t));
+
+    luaL_getmetatable(L, FUNCARRAY_USERDATA_METATABLE);
+    lua_setmetatable(L, -2);
+
+    lua_rawset(L, table);
+
+    return fa;
+}
+
+
+
+bool mrp_funcarray_call_from_c(lua_State *L,
+                               mrp_funcarray_t *fa,
+                               const char *signature,
+                               mrp_funcbridge_value_t *args)
+{
+    size_t i;
+    bool success, ok;
+    char rtyp;
+    mrp_funcbridge_value_t rval;
+
+    if (!fa || (fa->nfunc > 0 && !fa->funcs))
+        success = false;
+    else {
+        success = true;
+
+        for (i = 0;   i < fa->nfunc;   i++) {
+            ok = mrp_funcbridge_call_from_c(L, fa->funcs[i], signature, args,
+                                            &rtyp, &rval);
+            if (!ok || rtyp != MRP_FUNCBRIDGE_BOOLEAN || !rval.boolean)
+                success = false;
+        }
+    }
+
+    return success;
+}
+
+mrp_funcarray_t *mrp_funcarray_check(lua_State *L, int t)
+{
+    mrp_funcarray_t *fa;
+    size_t i, len;
+
+    if (t < 0 && t > LUA_REGISTRYINDEX)
+        t = lua_gettop(L) + t + 1;
+
+    switch (lua_type(L, t)) {
+
+    case LUA_TFUNCTION:
+        fa = mrp_funcarray_create(L);
+
+        fa->funcs = calloc(1, sizeof(mrp_funcbridge_t *));
+        fa->nfunc = 1;
+
+        fa->funcs[0] = mrp_funcbridge_create_luafunc(L, t);
+
+        break;
+
+    case LUA_TTABLE:
+        if (!(fa = to_funcarray(L, t))) {
+            fa = mrp_funcarray_create(L);
+
+            len = luaL_getn(L, t);
+
+            fa->funcs = calloc(len, sizeof(mrp_funcbridge_t *));
+            fa->nfunc = len;
+
+            for (i = 0;  i < len;  i++) {
+                lua_pushnumber(L, (int)(i + 1));
+                lua_gettable(L, t);
+
+                fa->funcs[i] = mrp_funcbridge_create_luafunc(L, -1);
+
+                lua_pop(L, 1);
+            }
+
+            lua_replace(L, t);
+        }
+        break;
+
+    default:
+        luaL_typerror(L, t, "function array");
+        fa = NULL;
+        break;
+    }
+
+    return fa;
+}
+
+
 static mrp_funcbridge_t *create_funcbridge(lua_State *L, int narr, int nrec)
 {
     int table;
@@ -303,7 +445,7 @@ static mrp_funcbridge_t *create_funcbridge(lua_State *L, int narr, int nrec)
     fb = (mrp_funcbridge_t *)lua_newuserdata(L,sizeof(mrp_funcbridge_t));
     memset(fb, 0, sizeof(mrp_funcbridge_t));
 
-    luaL_getmetatable(L, USERDATA_METATABLE);
+    luaL_getmetatable(L, FUNCBRIDGE_USERDATA_METATABLE);
     lua_setmetatable(L, -2);
 
     lua_rawset(L, table);
@@ -327,7 +469,7 @@ static mrp_funcbridge_t *check_funcbridge(lua_State *L, int t)
     lua_pushliteral(L, "userdata");
     lua_rawget(L, -2);
 
-    fb = (mrp_funcbridge_t *)luaL_checkudata(L, -1, USERDATA_METATABLE);
+    fb = luaL_checkudata(L, -1, FUNCBRIDGE_USERDATA_METATABLE);
     luaL_argcheck(L, fb != NULL, 1, "'function bridge' expected");
 
     lua_pop(L, 2);
@@ -335,11 +477,138 @@ static mrp_funcbridge_t *check_funcbridge(lua_State *L, int t)
     return fb;
 }
 
-static int call_from_lua(lua_State *L)
+static int call_funcbridge_from_lua(lua_State *L)
+{
+    mrp_funcbridge_t *fb = check_funcbridge(L, 1);
+
+    return make_lua_call(L, fb, 1);
+}
+
+static int get_funcbridge_field(lua_State *L)
+{
+    lua_pushnil(L);
+    return 1;
+}
+
+static int set_funcbridge_field(lua_State *L)
+{
+    return luaL_error(L, "attempt to write a readonly object");
+}
+
+static int funcbridge_destructor(lua_State *L)
+{
+    mrp_funcbridge_t *fb;
+
+    fb = luaL_checkudata(L, -1, FUNCBRIDGE_USERDATA_METATABLE);
+
+    if (!fb->dead) {
+        fb->dead = true;
+        mrp_funcbridge_unref(L, fb);
+    }
+
+    return 0;
+}
+
+static int call_funcarray_from_lua(lua_State *L)
+{
+    mrp_funcarray_t *fa;
+    int narg, top;
+    size_t i;
+    int j;
+    int success;
+
+    top  = lua_gettop(L);
+    narg = top - 1;
+
+    if (!(fa = to_funcarray(L, 1)))
+        luaL_typerror(L, 1, "function array");
+
+    if (fa->nfunc > 0 && !fa->funcs)
+        luaL_error(L, "attempt to call a corruptfunction array");
+
+    success = true;
+
+    for (i = 0;   i < fa->nfunc;   i++) {
+        mrp_funcbridge_push(L, fa->funcs[i]);
+
+        for (j = 0;  j < narg;  j++)
+            lua_pushvalue(L, j+2);
+
+        make_lua_call(L, fa->funcs[i], top+1);
+
+        if (!lua_isboolean(L, -1) || !lua_toboolean(L, -1))
+            success = false;
+
+        lua_settop(L, top);
+    }
+
+    lua_pushboolean(L, success);
+    lua_replace(L, 1);
+
+    lua_settop(L, 1);
+
+    return 1;
+}
+
+static int get_funcarray_field(lua_State *L)
+{
+    lua_pushnil(L);
+    return 1;
+}
+
+static int set_funcarray_field(lua_State *L)
+{
+    luaL_error(L, "attempt to change a function array");
+    return 0;
+}
+
+static mrp_funcarray_t *to_funcarray(lua_State *L, int t)
+{
+    mrp_funcarray_t *fa = NULL;
+
+    t = (t < 0) ? lua_gettop(L) + t + 1 : t;
+
+    if (t < 0 && t > LUA_REGISTRYINDEX)
+        t = lua_gettop(L) + t + 1;
+
+    if (lua_istable(L, t)) {
+        lua_pushstring(L, "userdata");
+        lua_rawget(L, t);
+
+        if (!lua_isnil(L, -1))
+            fa = luaL_checkudata(L, -1, FUNCARRAY_USERDATA_METATABLE);
+
+        lua_pop(L, 1);
+    }
+
+    return fa;
+}
+
+static int funcarray_destructor(lua_State *L)
+{
+    mrp_funcarray_t *fa;
+    size_t i;
+
+    fa = luaL_checkudata(L, -1, FUNCARRAY_USERDATA_METATABLE);
+
+    if (fa->funcs) {
+        for (i = 0;   i < fa->nfunc;   i++)
+            mrp_funcbridge_unref(L, fa->funcs[i]);
+
+        free(fa->funcs);
+    }
+
+    memset(fa, 0, sizeof(mrp_funcarray_t));
+
+    return 0;
+}
+
+
+
+static int make_lua_call(lua_State *L, mrp_funcbridge_t *fb, int f)
 {
 #define ARG_MAX 256
 
-    mrp_funcbridge_t *fb = check_funcbridge(L, 1);
     int ret;
     int i, n, m, b, e;
     const char *s;
@@ -347,8 +616,9 @@ static int call_from_lua(lua_State *L)
     mrp_funcbridge_value_t args[ARG_MAX];
     mrp_funcbridge_value_t *a, r;
 
-    b = 2;
     e = lua_gettop(L);
+    f = (f < 0) ? e + f + 1 : f;
+    b = f + 1;
     n = e - b + 1;
 
     switch (fb->type) {
@@ -408,10 +678,10 @@ static int call_from_lua(lua_State *L)
         break;
 
     case MRP_LUA_FUNCTION:
-        lua_rawgeti(L, 1, 1);
+        lua_rawgeti(L, f, 1);
         luaL_checktype(L, -1, LUA_TFUNCTION);
-        lua_replace(L, 1);
-        lua_call(L, n, 1);
+        lua_replace(L, f);
+        lua_pcall(L, n, 1, 0);
         ret = 1;
         break;
 
@@ -422,31 +692,6 @@ static int call_from_lua(lua_State *L)
     return ret;
 
 #undef ARG_MAX
-}
-
-static int get_funcbridge_field(lua_State *L)
-{
-    lua_pushnil(L);
-    return 1;
-}
-
-static int set_funcbridge_field(lua_State *L)
-{
-    return luaL_error(L, "attempt to write a readonly object");
-}
-
-static int funcbridge_destructor(lua_State *L)
-{
-    mrp_funcbridge_t *fb;
-
-    fb = (mrp_funcbridge_t *)luaL_checkudata(L, -1, USERDATA_METATABLE);
-
-    if (!fb->dead) {
-        fb->dead = true;
-        mrp_funcbridge_unref(L, fb);
-    }
-
-    return 0;
 }
 
 
