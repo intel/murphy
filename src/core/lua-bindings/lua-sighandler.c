2f043a4001f3fe42e2c10bad68b476ec5060aa3a
Krisztian Litkey <kli@iki.fi>
2014-10-26 15:14:42 +0200

    lua-sighandler: fixes, explicit enable/disable, removed 'data' member.
    Added support for explicitly enabling/disabling signal handlers.
    Removed 'userdata'-like data member as pointless. Since Lua
    sighander is an extensible object, one can set data as an extended
    Lua attribute just as well. No need to special-case it out...
    
    Signal handlers are now automatically disabled if invoking the
    associated callback fails.

--- src/core/lua-bindings/lua-sighandler.c
+++ src/core/lua-bindings/lua-sighandler.c
@@ -49,10 +49,10 @@
 
 typedef struct {
     lua_State        *L;                 /* Lua execution context */
+    mrp_mainloop_t   *ml;                /* Murphy mainloop */
     mrp_sighandler_t *h;                 /* associated murphy sighandler */
     int               signum;            /* signal number */
     int               callback;          /* reference to callback */
-    int               data;              /* referece to callback data */
     bool              oneshot;           /* true for one-shot sighandlers */
 } sighandler_lua_t;
 
@@ -62,7 +62,8 @@ static void sighandler_lua_destroy(void *data);
 static void sighandler_lua_changed(void *data, lua_State *L, int member);
 static ssize_t sighandler_lua_tostring(mrp_lua_tostr_mode_t mode, char *buf,
                                        size_t size, lua_State *L, void *data);
-
+static int sighandler_lua_enable(lua_State *L);
+static int sighandler_lua_disable(lua_State *L);
 
 /*
  * Lua sighandler class
@@ -74,7 +75,9 @@ static ssize_t sighandler_lua_tostring(mrp_lua_tostr_mode_t mode, char *buf,
 #define NOFLAGS MRP_LUA_CLASS_NOFLAGS
 
 MRP_LUA_METHOD_LIST_TABLE(sighandler_lua_methods,
-                          MRP_LUA_METHOD_CONSTRUCTOR(sighandler_lua_create));
+                          MRP_LUA_METHOD_CONSTRUCTOR(sighandler_lua_create)
+                          MRP_LUA_METHOD(disable, sighandler_lua_disable)
+                          MRP_LUA_METHOD(enable , sighandler_lua_enable));
 
 MRP_LUA_METHOD_LIST_TABLE(sighandler_lua_overrides,
                           MRP_LUA_OVERRIDE_CALL     (sighandler_lua_create));
@@ -82,21 +85,19 @@ MRP_LUA_METHOD_LIST_TABLE(sighandler_lua_overrides,
 MRP_LUA_MEMBER_LIST_TABLE(sighandler_lua_members,
     MRP_LUA_CLASS_INTEGER("signal"   , OFFS(signum)   , NULL, NULL, RDONLY )
     MRP_LUA_CLASS_LFUNC  ("callback" , OFFS(callback) , NULL, NULL, NOFLAGS)
-    MRP_LUA_CLASS_ANY    ("data"     , OFFS(data)     , NULL, NULL, NOFLAGS)
     MRP_LUA_CLASS_BOOLEAN("oneshot"  , OFFS(oneshot)  , NULL, NULL, NOFLAGS));
 
 typedef enum {
     SIGHANDLER_MEMBER_SIGNAL,
     SIGHANDLER_MEMBER_CALLBACK,
-    SIGHANDLER_MEMBER_DATA,
-    SIGHANDLER_MEMBER_DISABLED,
     SIGHANDLER_MEMBER_ONESHOT,
 } sighandler_member_t;
 
 MRP_LUA_DEFINE_CLASS(sighandler, lua, sighandler_lua_t, sighandler_lua_destroy,
                      sighandler_lua_methods, sighandler_lua_overrides,
                      sighandler_lua_members, NULL, sighandler_lua_changed,
-                     sighandler_lua_tostring, NULL, MRP_LUA_CLASS_EXTENSIBLE);
+                     sighandler_lua_tostring, NULL,
+                     MRP_LUA_CLASS_EXTENSIBLE | MRP_LUA_CLASS_DYNAMIC);
 
 
 static void sighandler_lua_cb(mrp_sighandler_t *hlr, int sig, void *user_data)
@@ -113,14 +114,15 @@ static void sighandler_lua_cb(mrp_sighandler_t *hlr, int sig, void *user_data)
             lua_pushstring(h->L, s);
         else
             lua_pushinteger(h->L, sig);
-        mrp_lua_object_deref_value(h, h->L, h->data, true);
 
         if (lua_pcall(h->L, 2, 0, 0) != 0)
             mrp_log_error("failed to invoke Lua sighandler callback");
     }
 
-    if (one)
+    if (one) {
         mrp_del_sighandler(h->h);
+        h->h = NULL;
+    }
 }
 
 
@@ -152,8 +154,8 @@ static int sighandler_lua_create(lua_State *L)
     h = (sighandler_lua_t *)mrp_lua_create_object(L, SIGHANDLER_LUA_CLASS,
                                                   NULL, 0);
     h->L        = L;
+    h->ml       = ctx->ml;
     h->callback = LUA_NOREF;
-    h->data     = LUA_NOREF;
 
     switch (narg) {
     case 1:
@@ -167,18 +169,12 @@ static int sighandler_lua_create(lua_State *L)
     }
 
     if (h->signum)
-        h->h = mrp_add_sighandler(ctx->ml, h->signum, sighandler_lua_cb, h);
-    else {
-        mrp_lua_destroy_object(L, NULL, 0, h);
+        h->h = mrp_add_sighandler(h->ml, h->signum, sighandler_lua_cb, h);
+    else
         return luaL_error(L, "signal number must be set in constructor");
-    }
 
-    if (h->h == NULL) {
-        mrp_lua_destroy_object(L, NULL, 0, h);
+    if (h->h == NULL)
         return luaL_error(L, "failed to create Murphy sighandler");
-    }
-
-    mrp_lua_push_object(L, h);
 
     return 1;
 }
@@ -188,13 +184,14 @@ static void sighandler_lua_destroy(void *data)
 {
     sighandler_lua_t *h = (sighandler_lua_t *)data;
 
+    mrp_debug("destroying Lua sighandler %p", data);
+
     mrp_del_sighandler(h->h);
     h->h = NULL;
 
     mrp_lua_object_unref_value(h, h->L, h->callback);
-    mrp_lua_object_unref_value(h, h->L, h->data);
+
     h->callback = LUA_NOREF;
-    h->data     = LUA_NOREF;
 }
 
 
@@ -212,7 +209,6 @@ static ssize_t sighandler_lua_tostring(mrp_lua_tostr_mode_t mode, char *buf,
     const char       *s = strsignal(h->signum);
 
     MRP_UNUSED(L);
-    MRP_UNUSED(sighandler_lua_check);
 
     switch (mode & MRP_LUA_TOSTR_MODEMASK) {
     case MRP_LUA_TOSTR_LUA:
@@ -222,6 +218,44 @@ static ssize_t sighandler_lua_tostring(mrp_lua_tostr_mode_t mode, char *buf,
                         s ? s : "unknow signal");
     }
 }
+
+
+static int sighandler_lua_enable(lua_State *L)
+{
+    sighandler_lua_t *h = sighandler_lua_check(L, -1);
+
+    if (h == NULL) {
+        lua_pushboolean(L, false);
+        return 1;
+    }
+
+    if (h->h == NULL && h->signum != 0)
+        if (h->callback != LUA_NOREF && h->callback != LUA_REFNIL)
+            mrp_add_sighandler(h->ml, h->signum, sighandler_lua_cb, h);
+
+    lua_pushboolean(L, h->h != NULL);
+
+    return 1;
+}
+
+
+static int sighandler_lua_disable(lua_State *L)
+{
+    sighandler_lua_t *h = sighandler_lua_check(L, -1);
+
+    if (h == NULL) {
+        lua_pushboolean(L, false);
+        return 1;
+    }
+
+    mrp_del_sighandler(h->h);
+    h->h = NULL;
+
+    lua_pushboolean(L, true);
+
+    return 1;
+}
+
 
 
 MURPHY_REGISTER_LUA_BINDINGS(murphy, SIGHANDLER_LUA_CLASS,
